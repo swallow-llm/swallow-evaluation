8ab16d307d1b44e99a2104685b3a9b17e4c6132d
stjohn2007 <stjohnsgus2007@gmail.com>
2024-01-26 00:19:30 +0900

    add: mt_bench script

---
+++ scripts/evaluate_ja_mt_bench.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# running lm-evaluation-harness-jp for wmt20 task
+source .venv_fastchat/bin/activate
+
+MODEL_NAME_PATH=$1
+
+OUTDIR="results/${MODEL_NAME_PATH}/ja/ja_mt_bench"
+mkdir -p ${OUTDIR}
+
+cd fastchat/fastchat/llm_judge
+python gen_model_answer.py --model-path ${MODEL_NAME_PATH} --model-id ${MODEL_NAME_PATH}
+python gen_judgment.py --model-list ${MODEL_NAME_PATH} --bench-name japanese_mt_bench --parallel 2
+python show_result.py --model-list ${MODEL_NAME_PATH} --bench-name japanese_mt_bench --output-file ../../../${OUTDIR}/judge.json
+cd ../../../
