54e65851c6baf00830313ea5b3dc579c1ccdfdf9
stjohn2007 <stjohnsgus2007@gmail.com>
2024-02-21 17:38:09 +0900

    add: humaneval

---
+++ bigcode-evaluation-harness/tmp.sh
@@ -0,0 +1,4 @@
+bash eval_en.sh tokyotech-llm/Mistral-7B-VE-algebraic-stack-the-vault-lr_2e-5-minlr_2e-6-iter0000500
+bash eval_en.sh tokyotech-llm/Mistral-7B-VE-algebraic-stack-the-vault-lr_2e-5-minlr_2e-6-iter0002000
+bash eval_en.sh tokyotech-llm/Mistral-7B-VE-algebraic-stack-the-vault-lr_2e-5-minlr_2e-6-iter0005000
+bash eval_en.sh tokyotech-llm/Mistral-7B-VE-algebraic-stack-the-vault-lr_2e-5-minlr_2e-6-iter0010000
---
+++ scripts/evaluate_ja_humaneval.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source .venv_bigcode/bin/activate
+
+
+MODEL_NAME_PATH=$1
+NUM_SAMPLES=10
+BATCH_SIZE=10
+OUTDIR="results/${MODEL_NAME_PATH}/ja/humaneval"
+
+mkdir -p $OUTDIR
+
+accelerate launch bigcode-evaluation-harness/main.py \
+  --model ${MODEL_NAME_PATH} \
+  --tasks jhumaneval \
+  --do_sample True \
+  --n_samples ${NUM_SAMPLES} \
+  --batch_size ${BATCH_SIZE} \
+  --allow_code_execution \
+  --save_generations \
+  --generation_only \
+  --save_generations_path ${OUTDIR}/generation.json \
+  --use_auth_token
