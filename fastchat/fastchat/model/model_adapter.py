63b8330ae1ce19f1092a19d4922dd1c0e061c360
stjohn2007 <stjohnsgus2007@gmail.com>
2024-03-19 21:17:42 +0900

    fix: multi gpu setting

--- fastchat/fastchat/model/model_adapter.py
+++ fastchat/fastchat/model/model_adapter.py
@@ -2175,7 +2175,7 @@ class SwallowAdapter(BaseModelAdapter):
     def load_model(self, model_path: str, from_pretrained_kwargs: dict):
         tokenizer = AutoTokenizer.from_pretrained(model_path)
         model = AutoModelForCausalLM.from_pretrained(
-            model_path, low_cpu_mem_usage=True, device_map="auto", **from_pretrained_kwargs,
+            model_path, **from_pretrained_kwargs,
         )
         return model, tokenizer
 
