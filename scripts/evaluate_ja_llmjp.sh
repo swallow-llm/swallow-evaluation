8348af0924fcb4c6bc9bb80dd4cd9e3880111bb4
stjohn2007 <stjohnsgus2007@gmail.com>
2024-04-19 23:03:07 +0900

    fix: script

--- scripts/evaluate_ja_llmjp.sh
+++ scripts/evaluate_ja_llmjp.sh
@@ -5,17 +5,30 @@ source .venv_llm_jp_eval/bin/activate
 
 MODEL_NAME_PATH=$1
 TOKENIZER_NAME_PATH=$2
-NUM_FEWSHOT=$3
-NUM_TESTCASE=$4
-DATASET_DIR="llm-jp-eval/datasets/evaluation/test"
+DATASET_DIR="llm-jp-eval/dataset/1.3.0/evaluation/test"
+NUM_TESTCASE=-1
+GENERAL_NUM_FEWSHOT=4
+JMMLU_NUM_FEWSHOT=5
 
-OUTDIR="results/${MODEL_NAME_PATH}/ja/llmjp_${NUM_FEWSHOT}shot_${NUM_TESTCASE}cases"
+GENERAL_OUTDIR="results/${MODEL_NAME_PATH}/ja/llmjp/${GENERAL_NUM_FEWSHOT}shot_${NUM_TESTCASE}cases"
+JMMLU_OUTDIR="results/${MODEL_NAME_PATH}/ja/llmjp/${JMMLU_NUM_FEWSHOT}shot_${NUM_TESTCASE}cases"
 
 python llm-jp-eval/scripts/evaluate_llm.py -cn config.yaml \
   model.pretrained_model_name_or_path=$MODEL_NAME_PATH \
   tokenizer.pretrained_model_name_or_path=$TOKENIZER_NAME_PATH \
-  max_num_samples=$NUM_TESTCASE \
-  target_dataset="all" \
-  num_few_shots=$NUM_FEWSHOT \
+  metainfo.max_num_samples=$NUM_TESTCASE \
+  target_dataset="[\"jamp\", \"janli\", \"jcommonsenseqa\", \"jnli\", \"jsem\", \"jsick\", \"jsquad\", \"jsts\", \"niilc\"]" \
+  metainfo.num_few_shots=$GENERAL_NUM_FEWSHOT \
   dataset_dir=$DATASET_DIR \
-  log_dir=$OUTDIR
+  log_dir=$GENERAL_OUTDIR \
+  wandb.run_name=llm_jp_eval_general
+
+python llm-jp-eval/scripts/evaluate_llm.py -cn config.yaml \
+  model.pretrained_model_name_or_path=$MODEL_NAME_PATH \
+  tokenizer.pretrained_model_name_or_path=$TOKENIZER_NAME_PATH \
+  metainfo.max_num_samples=$NUM_TESTCASE \
+  target_dataset="jmmlu" \
+  metainfo.num_few_shots=$JMMLU_NUM_FEWSHOT \
+  dataset_dir=$DATASET_DIR \
+  log_dir=$JMMLU_OUTDIR \
+  wandb.run_name=llm_jp_eval_jmmlu
