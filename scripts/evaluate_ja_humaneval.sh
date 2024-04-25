0492771a45259d34fc22225527759a240c0c1989
stjohn2007 <stjohnsgus2007@gmail.com>
2024-04-25 15:57:50 +0900

    fix: save evaluation results of humaneval

--- scripts/evaluate_ja_humaneval.sh
+++ scripts/evaluate_ja_humaneval.sh
@@ -3,24 +3,35 @@
 source .venv_bigcode/bin/activate
 
 MODEL_NAME_PATH=$1
+DO_GENERATION=$2
+DO_EVAL=$3
 NUM_SAMPLES=10
 BATCH_SIZE=10
 OUTDIR="results/${MODEL_NAME_PATH}/ja/humaneval"
 
 mkdir -p $OUTDIR
 
-#accelerate launch bigcode-evaluation-harness/main.py \
-python bigcode-evaluation-harness/main.py \
-  --model ${MODEL_NAME_PATH} \
-  --tasks jhumaneval \
-  --do_sample True \
-  --n_samples ${NUM_SAMPLES} \
-  --batch_size ${BATCH_SIZE} \
-  --allow_code_execution \
-  --save_generations \
-  --generation_only \
-  --save_generations_path ${OUTDIR}/generation.json \
-  --use_auth_token \
-  --max_memory_per_gpu auto \
-  --trust_remote_code \
-  --max_length_generation 1024
+if [ ${DO_GENERATION} = "true" ]; then
+  echo "Generating"
+  python bigcode-evaluation-harness/main.py \
+    --model ${MODEL_NAME_PATH} \
+    --tasks jhumaneval \
+    --do_sample True \
+    --n_samples ${NUM_SAMPLES} \
+    --batch_size ${BATCH_SIZE} \
+    --allow_code_execution \
+    --save_generations \
+    --generation_only \
+    --save_generations_path ${OUTDIR}/generation.json \
+    --use_auth_token \
+    --max_memory_per_gpu auto \
+    --trust_remote_code \
+    --max_length_generation 1024
+fi
+
+if [ ${DO_EVAL} = "true" ]; then
+  echo "Evaluating"
+  echo "Generated codes should be place at $(pwd)/${OUTDIR}/generation_jhumaneval.json ."
+  touch $(pwd)/${OUTDIR}/metrics.json
+  docker run -v $(pwd)/${OUTDIR}/generation_jhumaneval.json:/app/generations_py.json -v $(pwd)/${OUTDIR}/metrics.json:/app/metrics.json -it evaluation-harness python3 main.py --model ${MODEL_NAME_PATH} --tasks humaneval --load_generations_path /app/generations_py.json --allow_code_execution --n_samples 10 --metric_output_path /app/metrics.json
+fi
