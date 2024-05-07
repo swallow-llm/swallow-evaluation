587593306b342b7815a9e8dc6d145161d747540b
stjohn2007 <stjohnsgus2007@gmail.com>
2024-05-07 16:00:36 +0900

    fix: remove comment out

--- scripts/abci/environment/qsub_create_environment.sh
+++ scripts/abci/environment/qsub_create_environment.sh
@@ -5,5 +5,5 @@ mkdir -p logs
 qsub -g $GROUP_ID -o "$REPO_PATH/scripts/abci/environment/logs/" -e "$REPO_PATH/scripts/abci/environment/logs/" "$REPO_PATH/scripts/abci/environment/llm-jp-eval.sh" $REPO_PATH
 qsub -g $GROUP_ID -o "$REPO_PATH/scripts/abci/environment/logs/" -e "$REPO_PATH/scripts/abci/environment/logs/" "$REPO_PATH/scripts/abci/environment/lm-harness-jp.sh" $REPO_PATH
 qsub -g $GROUP_ID -o "$REPO_PATH/scripts/abci/environment/logs/" -e "$REPO_PATH/scripts/abci/environment/logs/" "$REPO_PATH/scripts/abci/environment/lm-harness-en.sh" $REPO_PATH
-#qsub -g $GROUP_ID -o "$REPO_PATH/scripts/abci/environment/logs/" -e "$REPO_PATH/scripts/abci/environment/logs/" "$REPO_PATH/scripts/abci/environment/bigcode.sh" $REPO_PATH
+qsub -g $GROUP_ID -o "$REPO_PATH/scripts/abci/environment/logs/" -e "$REPO_PATH/scripts/abci/environment/logs/" "$REPO_PATH/scripts/abci/environment/bigcode.sh" $REPO_PATH
 qsub -g $GROUP_ID -o "$REPO_PATH/scripts/abci/environment/logs/" -e "$REPO_PATH/scripts/abci/environment/logs/" "$REPO_PATH/scripts/abci/environment/fastchat.sh" $REPO_PATH
