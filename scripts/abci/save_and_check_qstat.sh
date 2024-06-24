59fc0051e720c970080e659ddc071a761a40e3f1
KoshiroSaito <twelverjunix@gmail.com>
2024-06-24 14:55:08 +0900

    changed save_and_check_qstat.sh to table format

--- scripts/abci/save_and_check_qstat.sh
+++ scripts/abci/save_and_check_qstat.sh
@@ -4,10 +4,10 @@
 cd "$(dirname "$0")"
 
 # 現在のジョブの状態を取得して一時ファイルに保存
-qstat -u $USER > current_qstat.tmp
+qstat -u $USER > current_qstat.out
 
 # 一時ファイルを解析してジョブIDと状態を取得
-current_jobs=$(awk 'NR>2 {print $1, $5}' current_qstat.tmp)
+current_jobs=$(awk 'NR>2 {print $1, $5}' current_qstat.out)
 
 # 結果を格納する変数を初期化
 results=""
@@ -24,27 +24,31 @@ while read -r job_id state; do
 
   # タスク名とモデル名が空でない場合のみ結果を蓄積
   if [[ -n "$task_kind" && -n "$model_name" ]]; then
-    results+="$job_id\t$state\t$task_kind\t$model_name\t$node_kind\n"
-    current_job_map["$job_id"]="$state $task_kind $model_name $node_kind"
+    results+="$job_id\t$state\t$task_kind\t$node_kind\t$model_name\n"
+    current_job_map["$job_id"]="$state $task_kind $node_kind $model_name"
   fi
 done <<< "$current_jobs"
+
+# 出力のヘッダー
+echo -e "job_ID\tstate\ttask\t\tnode\tmodel name "
+echo -e "----------------------------------------------------------------------------------------------------------------------------"
 
 # 前回のジョブの状態を読み込む
 if [ -f qstat_history.out ]; then
   old_jobs=$(cat qstat_history.out)
 
   # 前回のジョブIDが現在のジョブIDにない場合は終了したとみなす
-  echo "$old_jobs" | while read -r old_job_id old_state old_task_kind old_model_name old_node_kind; do
+  echo "$old_jobs" | while read -r old_job_id old_state old_task_kind old_node_kind old_model_name; do
     if [[ -z "${current_job_map[$old_job_id]}" ]]; then
-      echo -e "$old_job_id was done.\tTask: $old_task_kind\tNode: $old_node_kind\tModel: $old_model_name"
+      echo -e "$old_job_id done\t$old_task_kind\t$old_node_kind\t$old_model_name"
     fi
   done
 fi
 
 # 現在のジョブ情報を表示
-echo -e "$results" | while read -r job_id state task_kind model_name node_kind; do
+echo -e "$results" | while read -r job_id state task_kind node_kind model_name; do
   if [[ -n "$job_id" ]]; then
-    echo -e "$job_id is currently $state.\tTask: $task_kind\tNode: $node_kind\tModel: $model_name"
+    echo -e "$job_id $state\t$task_kind\t$node_kind\t$model_name"
   fi
 done
 
@@ -52,4 +56,4 @@ done
 echo -e "$results" > qstat_history.out
 
 # 一時ファイルを削除
-rm current_qstat.tmp
+rm current_qstat.out
