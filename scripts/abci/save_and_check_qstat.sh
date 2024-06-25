050b023d984e3921616079a2ab7819ba0675be0e
KoshiroSaito <twelverjunix@gmail.com>
2024-06-25 13:48:40 +0900

    added slots column

--- scripts/abci/save_and_check_qstat.sh
+++ scripts/abci/save_and_check_qstat.sh
@@ -23,22 +23,25 @@ declare -A current_job_map
 
 # 各ジョブについて情報を取得
 while read -r job_id state; do
-  task_kind=$(qstat -j "$job_id" | grep script_file | sed 's:.*/evaluate_::')
-  model_name=$(qstat -j "$job_id" | grep job_args | sed 's:.*/::' | sed 's/,.*//')
-  node_kind=$(qstat -j "$job_id" | grep ar_id | awk '{print $2}')
+  job_info=$(qstat -j "$job_id")
+  task_kind=$(echo "$job_info" | grep job_name | awk '{print $2}' | sed -e 's:evaluate_::' -e 's:.sh::')
+  model_name=$(echo "$job_info" | grep job_args | sed 's:.*/::' | sed 's/,.*//')
+  node_kind=$(echo "$job_info" | grep ar_ids | awk '{print $2}')
+  slots=$(echo "$job_info" | grep parallel | awk '{print $5}')
+  
   if [ -z "$node_kind" ]; then
     node_kind="normal"
   fi
 
   # タスク名とモデル名が空でない場合のみ結果を蓄積
   if [[ -n "$task_kind" && -n "$model_name" ]]; then
-    results+="$job_id\t$state\t$task_kind\t$node_kind\t$model_name\n"
-    current_job_map["$job_id"]="$state $task_kind $node_kind $model_name"
+    results+="$job_id\t$state\t$node_kind\t$slots\t$task_kind\t$model_name\n"
+    current_job_map["$job_id"]="$state $node_kind $slots $task_kind $model_name"
   fi
 done <<< "$current_jobs"
 
 # 出力のヘッダー
-printf "%-10s %-8s %-10s %-50s %-100s\n" "job_ID" "state" "node" "task" "model name"
+printf "%-10s %-8s %-10s %-8s %-50s %-100s\n" "job_ID" "state" "node" "slots" "task" "model name"
 echo -e "-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 
 # 前回のジョブの状態を読み込む
@@ -46,17 +49,17 @@ if [ -f qstat_history.out ]; then
   old_jobs=$(cat qstat_history.out)
 
   # 前回のジョブIDが現在のジョブIDにない場合は終了したとみなす
-  echo "$old_jobs" | while read -r old_job_id old_state old_task_kind old_node_kind old_model_name; do
+  echo "$old_jobs" | while read -r old_job_id old_state old_node_kind old_slots old_task_kind old_model_name; do
     if [[ -z "${current_job_map[$old_job_id]}" ]]; then
-      printf "%-10s %-8s %-10s %-50s %-100s\n" "$old_job_id" "done" "$old_node_kind" "$old_task_kind" "$old_model_name"
+      printf "%-10s %-8s %-10s %-8s %-50s %-100s\n" "$old_job_id" "done" "$old_node_kind" "$old_slots" "$old_task_kind" "$old_model_name"
     fi
   done
 fi
 
 # 現在のジョブ情報を表示
-echo -e "$results" | while read -r job_id state task_kind node_kind model_name; do
+echo -e "$results" | while read -r job_id state node_kind slots task_kind model_name; do
   if [[ -n "$job_id" ]]; then
-    printf "%-10s %-8s %-10s %-50s %-100s\n" "$job_id" "$state" "$node_kind" "$task_kind" "$model_name"
+    printf "%-10s %-8s %-10s %-8s %-50s %-100s\n" "$job_id" "$state" "$node_kind" "$slots" "$task_kind" "$model_name"
   fi
 done
 
