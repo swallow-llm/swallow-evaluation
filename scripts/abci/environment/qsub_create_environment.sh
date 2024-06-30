5a441ccac4b42fa517c50489fee9663a2330b4c7
stjohn2007 <stjohnsgus2007@gmail.com>
2024-06-30 12:35:58 +0900

    delete necessary files

--- scripts/abci/environment/qsub_create_environment.sh
+++
@@ -1,9 +0,0 @@
-REPO_PATH="/home/acb11709gz/jalm-evaluation-private"
-GROUP_ID="gag51395"
-mkdir -p logs
-
-qsub -g $GROUP_ID -o "$REPO_PATH/scripts/abci/environment/logs/" -e "$REPO_PATH/scripts/abci/environment/logs/" "$REPO_PATH/scripts/abci/environment/llm-jp-eval.sh" $REPO_PATH
-qsub -g $GROUP_ID -o "$REPO_PATH/scripts/abci/environment/logs/" -e "$REPO_PATH/scripts/abci/environment/logs/" "$REPO_PATH/scripts/abci/environment/lm-harness-jp.sh" $REPO_PATH
-qsub -g $GROUP_ID -o "$REPO_PATH/scripts/abci/environment/logs/" -e "$REPO_PATH/scripts/abci/environment/logs/" "$REPO_PATH/scripts/abci/environment/lm-harness-en.sh" $REPO_PATH
-qsub -g $GROUP_ID -o "$REPO_PATH/scripts/abci/environment/logs/" -e "$REPO_PATH/scripts/abci/environment/logs/" "$REPO_PATH/scripts/abci/environment/bigcode.sh" $REPO_PATH
-qsub -g $GROUP_ID -o "$REPO_PATH/scripts/abci/environment/logs/" -e "$REPO_PATH/scripts/abci/environment/logs/" "$REPO_PATH/scripts/abci/environment/fastchat.sh" $REPO_PATH
--- scripts/okazaki_lab/evaluate_en_tasks.sh
+++
@@ -1,8 +0,0 @@
-MODEL_NAME_PATH=$1
-CUDA_VISIBLE_DEVICES=$2
-GPU_NUM=$3
-
-export CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES
-
-bash scripts/evaluate_english_humaneval.sh $MODEL_NAME_PATH true true
-bash scripts/evaluate_english.sh $MODEL_NAME_PATH
