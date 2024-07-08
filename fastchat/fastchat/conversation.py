fa740d04099781478baff9643294b0c31a3c5810
stjohn2007 <stjohnsgus2007@gmail.com>
2024-07-08 14:12:44 +0900

    delete unnecessary lines

--- fastchat/fastchat/conversation.py
+++ fastchat/fastchat/conversation.py
@@ -379,7 +379,6 @@ def register_conv_template(template: Conversation, override: bool = False):
 
 def get_conv_template(name: str) -> Conversation:
     """Get a conversation template."""
-    print("Using template: ", name)
     return conv_templates[name].copy()
 
 
