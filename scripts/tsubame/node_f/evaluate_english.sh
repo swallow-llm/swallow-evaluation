0f414c97fb3861a9b129c6eda5af2abab50f755a
Taishi-N324 <taishiahiru5@gmail.com>
2024-08-11 15:02:51 +0900

    Update CUDA_BLOCKING sarashina2-use_fast_tokenizer

--- scripts/tsubame/node_f/evaluate_english.sh
+++ scripts/tsubame/node_f/evaluate_english.sh
@@ -12,9 +12,19 @@ module load cudnn/9.0.0
 REPO_PATH=$1
 HUGGINGFACE_CACHE=$2
 MODEL_NAME_PATH=$3
+CUDA_BLOCKING=${4:-}
 
 export HUGGINGFACE_HUB_CACHE=$HUGGINGFACE_CACHE
 export HF_HOME=$HUGGINGFACE_CACHE
+
+# Set CUDA_LAUNCH_BLOCKING to prevent evaluation from stopping at a certain batch
+# (This setting should be done only if necessary because it might slow evaluation)
+if [ -n "$CUDA_BLOCKING" ]; then
+  export CUDA_LAUNCH_BLOCKING=$CUDA_BLOCKING
+else
+  unset CUDA_LAUNCH_BLOCKING
+fi
+echo CUDA_LAUNCH_BLOCKING=$CUDA_BLOCKING
 
 cd $REPO_PATH
 
@@ -37,6 +47,13 @@ BBH_TASK_NAME="bbh_cot_fewshot"
 BBH_NUM_FEWSHOT=3
 BBH_NUM_TESTCASE="all"
 BBH_OUTDIR="${OUTDIR}/alltasks_${BBH_NUM_FEWSHOT}shot_${BBH_NUM_TESTCASE}cases/bbh_cot"
+
+# MODEL_NAME_PATHにsarashina2が含まれているとき,use_fast_tokenizer=Falseが指定される
+if [[ $MODEL_NAME_PATH == *"sarashina2"* ]]; then
+    USE_FAST_TOKENIZER=False
+else
+    USE_FAST_TOKENIZER=True
+fi
 
 mkdir -p $GENERAL_OUTDIR
 mkdir -p $MMLU_OUTDIR
