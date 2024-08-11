b4c32ad5af3e14b866c47858be17f288c7215a05
Taishi-N324 <taishiahiru5@gmail.com>
2024-08-11 14:52:32 +0900

    Update CUDA_BLOCKING sarashina2-use_fast_tokenizer

--- scripts/tsubame/node_q/evaluate_english_humaneval-unstripped.sh
+++ scripts/tsubame/node_q/evaluate_english_humaneval-unstripped.sh
@@ -14,9 +14,19 @@ HUGGINGFACE_CACHE=$2
 MODEL_NAME_PATH=$3
 DO_GENERATION=$4
 DO_EVAL=$5
+CUDA_BLOCKING=${6:-}
 
 export HUGGINGFACE_HUB_CACHE=$HUGGINGFACE_CACHE
 export HF_HOME=$HUGGINGFACE_CACHE
+
+# Set CUDA_LAUNCH_BLOCKING to prevent evaluation from stopping at a certain batch
+# (This setting should be done only if necessary because it might slow evaluation)
+if [ -n "$CUDA_BLOCKING" ]; then
+  export CUDA_LAUNCH_BLOCKING=$CUDA_BLOCKING
+else
+  unset CUDA_LAUNCH_BLOCKING
+fi
+echo CUDA_LAUNCH_BLOCKING=$CUDA_BLOCKING
 
 cd $REPO_PATH
 
@@ -26,6 +36,14 @@ NUM_SAMPLES=10
 BATCH_SIZE=10
 OUTDIR="${REPO_PATH}/results/${MODEL_NAME_PATH}/en/humaneval-unstripped"
 APPTAINER_IMAGE="evaluation-harness_latest.sif"
+
+# MODEL_NAME_PATHにsarashina2が含まれているとき,use_fast_tokenizer=Falseが指定される
+if [[ $MODEL_NAME_PATH == *"sarashina2"* ]]; then
+    USE_FAST_TOKENIZER=''
+else
+    USE_FAST_TOKENIZER='--use_fast_tokenizer'
+fi
+
 
 mkdir -p $OUTDIR
 
@@ -43,7 +61,8 @@ if [ ${DO_GENERATION} = "true" ]; then
     --use_auth_token \
     --max_memory_per_gpu auto \
     --trust_remote_code \
-    --max_length_generation 1024
+    --max_length_generation 1024 \
+    ${USE_FAST_TOKENIZER}
 fi
 
 if [ ${DO_EVAL} = "true" ]; then
