18ac3de7b01c721bc4af08970e010a301fe4cb13
Koshiro Saito <twelverjunix@gmail.com>
2024-11-12 16:32:50 +0900

    added vllm scripts for bigbench

---
+++ scripts/tsubame/environment/bigcode.sh
@@ -0,0 +1,22 @@
+REPO_PATH="/gs/fs/tga-okazaki/saito/jalm-evaluation-private"
+
+####
+
+cd $REPO_PATH
+
+module load cuda/12.1.0
+module load cudnn/9.0.0
+
+python -m venv .venv_bigcode
+
+source .venv_bigcode/bin/activate
+cd bigcode-evaluation-harness
+pip install --upgrade pip
+pip install -e .
+pip install sentencepiece protobuf transformers
+pip install 'accelerate>=0.26.0'
+pip install datasets==2.21.0
+pip install vllm
+deactivate
+
+cd $REPO_PATH
