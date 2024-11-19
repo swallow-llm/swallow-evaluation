ed814fa9c8b0bd8ca77a1f369899fb8867c68c35
Koshiro Saito <twelverjunix@gmail.com>
2024-11-19 20:40:46 +0900

    specified openai==0.28

--- scripts/tsubame/environment/fastchat.sh
+++ scripts/tsubame/environment/fastchat.sh
@@ -21,8 +21,8 @@ cd fastchat
 pip install --upgrade pip
 pip install python-dotenv pandas
 pip install -e ".[model_worker,llm_judge]"
-pip install openai==0.28
 pip install vllm==v0.6.3.post1
+pip install openai==0.28
 deactivate
 
 cd $REPO_PATH
