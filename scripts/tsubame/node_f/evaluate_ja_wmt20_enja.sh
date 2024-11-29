2e9c8bf938e0b61e260a7745160272cf0cf0dd74
Koshiro Saito <twelverjunix@gmail.com>
2024-11-29 12:30:28 +0900

    fix: add bos token for gemma

--- scripts/tsubame/node_f/evaluate_ja_wmt20_enja.sh
+++ scripts/tsubame/node_f/evaluate_ja_wmt20_enja.sh
@@ -32,6 +32,7 @@ source .venv_harness_jp/bin/activate
 NUM_FEWSHOT=4
 NUM_TESTCASE="all"
 OUTDIR="${REPO_PATH}/results/${MODEL_NAME_PATH}/ja/wmt20_en_ja/wmt20_en_ja_${NUM_FEWSHOT}shot_${NUM_TESTCASE}cases"
+
 # MODEL_NAME_PATHにsarashina2が含まれているとき,use_fast=Falseが指定される
 if [[ $MODEL_NAME_PATH == *"sarashina2"* ]]; then
     USE_FAST_TOKENIZER=False
@@ -39,11 +40,18 @@ else
     USE_FAST_TOKENIZER=True
 fi
 
+# MODEL_NAME_PATHにgemma-2が含まれているとき,add_special_tokens=Trueが指定される
+if [[ $MODEL_NAME_PATH == *"gemma-2"* ]]; then
+    ADD_SPECIAL_TOKENS=True
+else
+    ADD_SPECIAL_TOKENS=False
+fi
+
 mkdir -p ${OUTDIR}
 
 python lm-evaluation-harness-jp/main.py \
     --model hf-causal-experimental \
-    --model_args "pretrained=$MODEL_NAME_PATH,use_accelerate=True,trust_remote_code=True,use_fast=$USE_FAST_TOKENIZER" \
+    --model_args "pretrained=$MODEL_NAME_PATH,use_accelerate=True,trust_remote_code=True,use_fast=$USE_FAST_TOKENIZER,add_special_tokens=$ADD_SPECIAL_TOKENS" \
     --tasks "wmt20-en-ja" \
     --num_fewshot $NUM_FEWSHOT \
     --batch_size 2 \
