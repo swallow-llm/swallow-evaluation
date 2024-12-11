94914ec3b416ab269b61a6f99b8c09170881e30d
saito.k <saito.k.ch@m.titech.ac.jp>
2024-12-12 08:34:47 +0900

    RE: removed scripts to install unused libraries

--- scripts/tsubame/environment/fastchat.sh
+++ scripts/tsubame/environment/fastchat.sh
@@ -27,7 +27,7 @@ pip install --upgrade pip
 pip install python-dotenv pandas
 pip install -e ".[model_worker,llm_judge]"
 pip install vllm==v0.6.3.post1
-pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cu121
+pip install torch==2.4.0 --index-url https://download.pytorch.org/whl/cu121
 deactivate
 
 cd $REPO_PATH
--- scripts/tsubame/environment/lm-harness-en.sh
+++ scripts/tsubame/environment/lm-harness-en.sh
@@ -30,7 +30,7 @@ pip install sentencepiece==0.2.0 protobuf==5.28.3 transformers==4.46.2
 pip install 'accelerate>=0.26.0'
 pip install datasets==2.21.0
 pip install vllm==v0.6.3.post1
-pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cu121
+pip install torch==2.4.0 --index-url https://download.pytorch.org/whl/cu121
 deactivate
 
 cd $REPO_PATH
