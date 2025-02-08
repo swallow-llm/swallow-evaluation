0b09697ffbbe9c27caec9c3b6179f7a44e00b490
pakapaka333 <y_sakoro@yahoo.co.jp>
2025-02-08 16:56:07 +0900

    added env-setup scripts and eval scripts for bigcode for small-medium size models

--- scripts/abci/environment/qsub_create_environment.sh
+++ scripts/abci/environment/qsub_create_environment.sh
@@ -1,9 +1,7 @@
-REPO_PATH="/home/acb11709gz/jalm-evaluation-private"
+REPO_PATH="/home/acg16653re/github/jalm-evaluation-private_2412_update-saito_dev_abci"
+PIP_CACHEDIR="/home/acg16653re/my_cache/pip_cache"
+SINGULARITY_CACHEDIR="/home/acg16653re/my_cache/singularity_cache"
 GROUP_ID="gag51395"
 mkdir -p logs
 
-qsub -g $GROUP_ID -o "$REPO_PATH/scripts/abci/environment/logs/" -e "$REPO_PATH/scripts/abci/environment/logs/" "$REPO_PATH/scripts/abci/environment/llm-jp-eval.sh" $REPO_PATH
-qsub -g $GROUP_ID -o "$REPO_PATH/scripts/abci/environment/logs/" -e "$REPO_PATH/scripts/abci/environment/logs/" "$REPO_PATH/scripts/abci/environment/lm-harness-jp.sh" $REPO_PATH
-qsub -g $GROUP_ID -o "$REPO_PATH/scripts/abci/environment/logs/" -e "$REPO_PATH/scripts/abci/environment/logs/" "$REPO_PATH/scripts/abci/environment/lm-harness-en.sh" $REPO_PATH
-qsub -g $GROUP_ID -o "$REPO_PATH/scripts/abci/environment/logs/" -e "$REPO_PATH/scripts/abci/environment/logs/" "$REPO_PATH/scripts/abci/environment/bigcode.sh" $REPO_PATH
-qsub -g $GROUP_ID -o "$REPO_PATH/scripts/abci/environment/logs/" -e "$REPO_PATH/scripts/abci/environment/logs/" "$REPO_PATH/scripts/abci/environment/fastchat.sh" $REPO_PATH
+qsub -P $GROUP_ID -q rt_HC -l select=1 -l walltime=01:00:00 -N .SE_crtenv_bigcode -k oe -- "$REPO_PATH/scripts/abci/environment/bigcode.sh" $REPO_PATH $PIP_CACHEDIR $SINGULARITY_CACHEDIR
