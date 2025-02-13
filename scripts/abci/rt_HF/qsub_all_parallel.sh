f2aeef736e8a00711bd477238afd12564c8ce1cd
pakapaka333 <y_sakoro@yahoo.co.jp>
2025-02-13 18:28:13 +0900

    fix typo

--- scripts/abci/rt_HF/qsub_all_parallel.sh
+++ scripts/abci/rt_HF/qsub_all_parallel.sh
@@ -6,14 +6,14 @@ GROUP_ID="gag51395"
 
 MODEL_NAME_PATH=$1
 
-# group_1: llmjp, wmt20enja, wmt20jaen, mt_bench
-qsub -P $GROUP_ID -q rt_HF -l select=1 -l walltime=12:00:00 -N .SE_${MODEL_NAME_PATH//\//_}_GROUP_1 -k oe -- "$ROOT_PATH/scripts/abci/rt_HF/groups/evaluate_group_1.sh" $ROOT_PATH $HUGGINGFACE_CACHE $MODEL_NAME_PATH
+# # group_1: llmjp, wmt20enja, wmt20jaen, mt_bench
+# qsub -P $GROUP_ID -q rt_HF -l select=1 -l walltime=12:00:00 -N .SE_${MODEL_NAME_PATH//\//_}_GROUP_1 -k oe -- "$ROOT_PATH/scripts/abci/rt_HF/groups/evaluate_group_1.sh" $ROOT_PATH $HUGGINGFACE_CACHE $MODEL_NAME_PATH
 
-# group_2: mgsm, xlsum, jhumaneval-unstripped, mbpp-ja
-qsub -P $GROUP_ID -q rt_HF -l select=1 -l walltime=12:00:00 -N .SE_${MODEL_NAME_PATH//\//_}_GROUP_2 -k oe -- "$ROOT_PATH/scripts/abci/rt_HF/groups/evaluate_group_2.sh" $ROOT_PATH $HUGGINGFACE_CACHE $MODEL_NAME_PATH
+# # group_2: mgsm, xlsum, jhumaneval-unstripped, mbpp-ja
+# qsub -P $GROUP_ID -q rt_HF -l select=1 -l walltime=12:00:00 -N .SE_${MODEL_NAME_PATH//\//_}_GROUP_2 -k oe -- "$ROOT_PATH/scripts/abci/rt_HF/groups/evaluate_group_2.sh" $ROOT_PATH $HUGGINGFACE_CACHE $MODEL_NAME_PATH
 
 # group_3: en_general, mmlu, bbh, math
 qsub -P $GROUP_ID -q rt_HF -l select=1 -l walltime=12:00:00 -N .SE_${MODEL_NAME_PATH//\//_}_GROUP_3 -k oe -- "$ROOT_PATH/scripts/abci/rt_HF/groups/evaluate_group_3.sh" $ROOT_PATH $HUGGINGFACE_CACHE $MODEL_NAME_PATH
 
-# group_4: gpqa, humaneval-unstripped, mbpp
-qsub -P $GROUP_ID -q rt_HF -l select=1 -l walltime=12:00:00 -N .SE_${MODEL_NAME_PATH//\//_}_GROUP_4 -k oe -- "$ROOT_PATH/scripts/abci/rt_HF/groups/evaluate_group_4.sh" $ROOT_PATH $HUGGINGFACE_CACHE $MODEL_NAME_PATH
+# # group_4: gpqa, humaneval-unstripped, mbpp
+# qsub -P $GROUP_ID -q rt_HF -l select=1 -l walltime=12:00:00 -N .SE_${MODEL_NAME_PATH//\//_}_GROUP_4 -k oe -- "$ROOT_PATH/scripts/abci/rt_HF/groups/evaluate_group_4.sh" $ROOT_PATH $HUGGINGFACE_CACHE $MODEL_NAME_PATH
