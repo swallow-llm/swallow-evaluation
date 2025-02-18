2feca71abffb108d4aa28a1ea0eadf2b4ac2bfcc
pakapaka333 <y_sakoro@yahoo.co.jp>
2025-02-18 15:45:52 +0900

    doubled gpus for xl-sum

--- scripts/abci/rt_HF/groups/evaluate_group_3.sh
+++ scripts/abci/rt_HF/groups/evaluate_group_3.sh
@@ -17,26 +17,18 @@ export HF_HOME=$HUGGINGFACE_CACHE
 
 mkdir -p "$REPO_PATH/results/$MODEL_NAME_PATH/en/harness_en/"
 
-<<<<<<< Updated upstream
-=======
-# # GPUモニタリングをバックグラウンドで実行
-# python3 "$REPO_PATH/scripts/monitor_gpu.py" --output_path ~/.SE_${MODEL_NAME_PATH//\//_}_GPU_USAGE_GROUP_3_${PBS_JOBID}.csv > /dev/stdout 2>/dev/null &
-# MONITOR_PID=$!   # ここでPIDを取得しておく
-
-
->>>>>>> Stashed changes
 # 並列処理でスクリプトを実行
-CUDA_VISIBLE_DEVICES=0,1 bash "$REPO_PATH/scripts/abci/rt_HF/evaluate_ja_xlsum.sh" \
+CUDA_VISIBLE_DEVICES=0,1,2,3 bash "$REPO_PATH/scripts/abci/rt_HF/evaluate_ja_xlsum.sh" \
     "$REPO_PATH" "$HUGGINGFACE_CACHE" "$MODEL_NAME_PATH" \
     >  "$REPO_PATH/results/$MODEL_NAME_PATH/ja/xlsum/${PBS_JOBID}.o" \
     2> "$REPO_PATH/results/$MODEL_NAME_PATH/ja/xlsum/${PBS_JOBID}.e" &
 
-CUDA_VISIBLE_DEVICES=2,3 bash "$REPO_PATH/scripts/abci/rt_HF/evaluate_english_gpqa.sh" \
+CUDA_VISIBLE_DEVICES=4,5 bash "$REPO_PATH/scripts/abci/rt_HF/evaluate_english_gpqa.sh" \
     "$REPO_PATH" "$HUGGINGFACE_CACHE" "$MODEL_NAME_PATH" \
     >  "$REPO_PATH/results/$MODEL_NAME_PATH/en/harness_en/gpqa_${PBS_JOBID}.o" \
     2> "$REPO_PATH/results/$MODEL_NAME_PATH/en/harness_en/gpqa_${PBS_JOBID}.e" &
 
-CUDA_VISIBLE_DEVICES=4,5 bash "$REPO_PATH/scripts/abci/rt_HF/evaluate_english_math.sh" \
+CUDA_VISIBLE_DEVICES=6,7 bash "$REPO_PATH/scripts/abci/rt_HF/evaluate_english_math.sh" \
     "$REPO_PATH" "$HUGGINGFACE_CACHE" "$MODEL_NAME_PATH" \
     >  "$REPO_PATH/results/$MODEL_NAME_PATH/en/harness_en/math_${PBS_JOBID}.o" \
     2> "$REPO_PATH/results/$MODEL_NAME_PATH/en/harness_en/math_${PBS_JOBID}.e" &
@@ -44,9 +36,3 @@ CUDA_VISIBLE_DEVICES=4,5 bash "$REPO_PATH/scripts/abci/rt_HF/evaluate_english_ma
 
 # すべてのジョブが終了するまで待つ
 wait
-<<<<<<< Updated upstream
-=======
-
-# # GPUモニタリングを停止
-# kill "$MONITOR_PID"
->>>>>>> Stashed changes
